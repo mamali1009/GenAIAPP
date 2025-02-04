64dd262c813186d34c9047684b58ae7937c57164
mamali1009 <mohammed.abdulmueedali@cognizant.com>
2025-02-04 21:12:08 +0530

    Delete scripts directory

--- scripts/install_dependencies.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user/GenAIAPP
-pip install -r requirements.txt
--- scripts/start_streamlit.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-pkill -f streamlit
-nohup streamlit run /home/ec2-user/GenAIAPP/Streamlit2.py --server.port 8501 --server.enableCORS false > /home/ec2-user/GenAIAPP/streamlit.log 2>&1 &
